025c13914fb6034d7d410cb94904330e4ebfea15
Lily Gates <>
2025-11-17 23:29:09 -0500

    Add new API script for Bureau of Labor Statistics (BLS)

---
+++ bls_api.py
@@ -0,0 +1,30 @@
+# The Bureau of Labor Statistics (BLS) API
+# Written by Lily Gates
+# Created: 11/17/2025
+# Last Updated: 11/17/2025 
+
+# Things to instal if haven't already (only once)
+
+import requests
+import json
+import prettytable
+headers = {'Content-type': 'application/json'}
+data = json.dumps({"seriesid": ['CUUR0000SA0','SUUR0000SA0'],"startyear":"2011", "endyear":"2014"})
+p = requests.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', data=data, headers=headers)
+json_data = json.loads(p.text)
+for series in json_data['Results']['series']:
+    x=prettytable.PrettyTable(["series id","year","period","value","footnotes"])
+    seriesId = series['seriesID']
+    for item in series['data']:
+        year = item['year']
+        period = item['period']
+        value = item['value']
+        footnotes=""
+        for footnote in item['footnotes']:
+            if footnote:
+                footnotes = footnotes + footnote['text'] + ','
+        if 'M01' <= period <= 'M12':
+            x.add_row([seriesId,year,period,value,footnotes[0:-1]])
+    output = open(seriesId + '.txt','w')
+    output.write (x.get_string())
+    output.close()
